1857fef61e1c5e6853198cc60fe40a2a0aa22af5
tedder <ted@adhoclabs.co>
2013-03-13 11:16:57 -0700

    can haz xml

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python
 
-import srs
+import sys
+import os.path
+import xml.etree.ElementTree as xml
 
-for arg in sys.argv:
+def readfile(fh):
+	xmltree = xml.parse(fh)
+	print "xml: %s" % xml
+
+files = []
+for arg in sys.argv[1:]:
 	print "arg: %s" % arg
+	if os.path.isfile(arg):
+		files.append(arg)
+	else:
+		print "argument doesn't appear to be a file: %s" % arg
+
+print files
+
+for file in files:
+	fh = open(file)
+	print "fh: %s" % fh
+	readfile(fh)
+
