6f64f29cba4eb5e76f7f962dd3c38ff9fb9c9a18
tedder <ted@adhoclabs.co>
2013-03-14 13:35:59 -0400

    incremental, but the prefixed entities fail.

--- src/parser.py
+++ src/parser.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python
+#coding: utf-8
 
 import sys
 import os.path
-import xml.etree.ElementTree as xml
+import xml.etree.cElementTree as xml
 
-def readfile(fh):
+def readfile(fh=None):
 	xmltree = xml.parse(fh)
-	print "xml: %s" % xml
+
+	#print "prefixed entity should work: %s" % xmltree.find("cato:entity")
+	# SyntaxError: prefix 'cato' not found in prefix map
+
+	print "prefixed entity should work: %s" % xmltree.getroot().find("{http://namespaces.cato.org/catoxml}entity/")
+	print "prefixed entity should work: %s" % xmltree.getroot().findall("{http://namespaces.cato.org/catoxml}entity/")
+	print "prefixed entity should work: %s" % xmltree.getroot().find("{http://namespaces.cato.org/catoxml}entity")
+	print "prefixed entity should work: %s" % xmltree.getroot().findall("{http://namespaces.cato.org/catoxml}entity")
+	#er = xmltree.find("entity-ref")
+	print "this should have something: %s" % xmltree.getroot().findall("*/entity")
+
+
+	print "this should iterate too:"
+	for e in xmltree.getroot().iter("entity"):
+		print "e: %s" % e
+
+	#print "this dumps elements so we can see what they contain"
+	#for e in xmltree.getroot().iter():
+		#print "e: %s/%s" % (e.tag, "")
 
 files = []
 for arg in sys.argv[1:]:
